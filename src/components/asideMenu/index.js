bec398282e2d701d0ebd6e31cfedb6c07bd65ce5
kn5886348135 <kn5886348135@163.com>
2020-10-21 21:47:29 +0800

    菜单选中和高亮

--- src/components/asideMenu/index.js
+++ src/components/asideMenu/index.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react';
-import { Link } from 'react-router-dom'
+import { Link,withRouter } from 'react-router-dom'
 import './aside.scss'
 import { Menu } from 'antd'
 import {
@@ -20,11 +20,46 @@ class AsideMenu extends Component {
     constructor(props){
         super(props)
         this.state = {
-
+            selectedKeys:['/index/user/list'],
+            openKeys:['/index/user']
         }
         console.log(Router)
     }
 
+    componentDidMount(){
+        const pathname = this.props.location.pathname
+        const menuKey = pathname.split('/').slice(0,3).join('/')
+        
+        const menuHeight = {
+            selectedKeys: pathname,
+            openKeys: menuKey
+        }
+
+        this.menuHeightLight(menuHeight)
+    }
+
+    selectMenu = ({ item, key, keyPath, domEvent}) => {
+        const menuKey = keyPath[keyPath.length -1]
+        
+        const menuHeight = {
+            selectedKeys: key,
+            openKeys: menuKey
+        }
+        this.menuHeightLight(menuHeight)
+    }
+
+    // menuHeightLight = (params) => {
+    //     this.setState({
+    //         selectedKeys: [params.selectedKeys],
+    //         openKeys: [params.openKeys]
+    //     })
+    // }
+    menuHeightLight = ({selectedKeys, openKeys}) => {
+        this.setState({
+            selectedKeys: [selectedKeys],
+            openKeys: [openKeys]
+        })
+    }
     renderSubMenu = ({title, path, children}) => {
         return (
             <SubMenu key={path} icon={<UserOutlined />} title={title}>
@@ -48,11 +83,13 @@ class AsideMenu extends Component {
     }
 
     render(h) {
+        const { selectedKeys, openKeys } = this.state
         return (
             <Fragment >
                <Menu
-                defaultSelectedKeys={['1']}
-                defaultOpenKeys={['sub1']}
+               onClick={this.selectMenu}
+                selectedKeys={selectedKeys}
+                openKeys={openKeys}
                 mode="inline"
                 theme="dark"
                 inlineCollapsed={this.state.collapsed}
@@ -68,4 +105,4 @@ class AsideMenu extends Component {
     }
 }
 
-export default AsideMenu
+export default withRouter(AsideMenu)
