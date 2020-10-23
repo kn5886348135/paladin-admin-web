426ca72d43801909a4750d2650f248fd3085313c
kn5886348135 <kn5886348135@163.com>
2020-10-23 21:42:34 +0800

    删除功能，复选框

--- src/api/department.js
+++ src/api/department.js
@@ -8,9 +8,17 @@ export function DepartmentAddApi(data){
     })
 }
 
-export function DepartmentListApi(data){
+export function GetDepartmentListApi(data){
     return service.request({
         url:'/department/list',
+        method: 'post',
+        data
+    })
+}
+
+export function DepartmentDeleteApi(data){
+    return service.request({
+        url:'/department/delete',
         method: 'post',
         data
     })
--- src/views/department/list.js
+++ src/views/department/list.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react';
-import { Form, Input, Button, Table } from 'antd'
+import { Form, Input, Button, Table, Switch } from 'antd'
+import { GetDepartmentListApi, DepartmentDeleteApi } from '../../api/department'
 
 class DepartmentList extends Component {
     constructor(props){
@@ -9,34 +10,85 @@ class DepartmentList extends Component {
                 {
                 title:'部门名称',dataIndex:'name',key:'name'
             },{
-                title:'禁启用',dataIndex:'status',key:'status'
+                title:'禁启用',dataIndex:'status',key:'status',render: (text, rowData) => {
+                    return <Switch checkedChildren="启用" unCheckedChildren="禁用" defaultChecked={ rowData.status === 1 ? true : false } />
+                }
             },{
                 title:'人员数量',dataIndex:'number',key:'number'
             },{
                 title:'描述',dataIndex:'conetnt',key:'conetnt'
             },{
-                title:'操作',dataIndex:'operation',key:'operation',width:'200px'
+                title:'操作',dataIndex:'operation',key:'operation',width:'200px',render: (text, rowData) => {
+                    return (
+                        <div className="inline-button">
+                            <Button type="primary">编辑</Button>
+                            <Button onClick={() => this.onHandlerDelete} type="primary">删除</Button>
+                        </div>
+                    )
+                }
             }
         ],
-        data:[{
-            key: '1',
-            name: '胡彦斌',
-            number: 32,
-            status: true
-          },
-          {
-            key: '2',
-            name: '胡彦祖',
-            number: 42,
-            status: true
-          }]
+        data:[],
+          pageNumber:1,
+          pageSize:10,
+          keyword:'',
+          selectedRowKeys:[]
         }
     }
 
     search = (value) => {
+        this.setState({
+            keyword:value.name,
+            pageNumber:1,
+            pageSize:10
+        })
+        this.loadData()
         console.log(value)
     }
+
+    componentDidMount(){
+        this.loadData()
+    }
+
+    loadData = () => {
+        const { pageNumber, pageSize, keyword } = this.state
+        const param = {
+            pageNumber: pageNumber,
+            pageSize: pageSize
+        }
+        if (keyword) {
+            param.name=keyword
+        }
+        GetDepartmentListApi().then(res => {
+            console.log(res)
+            const data = res.data
+            if (data.resCode === 0) {
+                
+            }
+        })
+    }
+
+    onHandlerDelete = (id) => {
+        if (!id) {
+            return false
+        }
+        DepartmentDeleteApi().then(res => {
+            console.log(res)
+            message.info(res.data.message)
+            this.loadData()
+        })
+        
+    }
+    onCheckbox = (selectedRowKeys) => {
+        this.setState({
+            selectedRowKeys: selectedRowKeys
+        })
+    }
     render(h) {
+        const { columns, data } = this.state
+        const rowSelection = {
+            onChange: this.onCheckbox
+        }
         return (
             <Fragment>
                 <Form layout="inline" onFinish={this.search}>
@@ -45,14 +97,14 @@ class DepartmentList extends Component {
                         label="部门名称"
                         rules={[{required: false, message:'请输入部门名称'}]}>
                             <Input placeholder='部门名称'/>
-                        </Form.Item>
+                    </Form.Item>
                     <Form.Item>
                         <Button type="primary" htmlType="submit">搜索</Button>
                     </Form.Item>
                     <Form.Item></Form.Item>
                     <Form.Item></Form.Item>
                 </Form>
-                <Table columns={this.state.columns} dataSource={this.state.data} bordered></Table>
+                <Table rowSelection={rowSelection} rowKey="id" columns={columns} dataSource={data} bordered></Table>
             </Fragment>
         )
     }
