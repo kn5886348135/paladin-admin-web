b10711956657dea119890d45798995a1f8a7a882
xunian <xngs1507179@163.com>
2021-02-04 22:50:13 +0800

    job功能

---
+++ src/api/job.js
@@ -0,0 +1,59 @@
+import service from '../utils/request';
+
+export function DepartmentAddApi(data){
+    return service.request({
+        url:'/department/add',
+        method: 'post',
+        data
+    })
+}
+
+export function DepartmentAddFormApi(data){
+    return service.request({
+        url:'/department/addform',
+        method: 'post',
+        data
+    })
+}
+
+export function GetDepartmentListApi(data){
+    return service.request({
+        url:'/department/list',
+        method: 'post',
+        data
+    })
+}
+
+
+
+export function DepartmentDeleteApi(data){
+    return service.request({
+        url:'/department/delete',
+        method: 'post',
+        data
+    })
+}
+
+export function ChangeStatusApi(data){
+    return service.request({
+        url:'/department/status',
+        method: 'post',
+        data
+    })
+}
+
+export function DepartmentDetailApi(data){
+    return service.request({
+        url:'/department/detailed',
+        method: 'post',
+        data
+    })
+}
+
+export function DepartmentEditApi(data){
+    return service.request({
+        url:'/department/edit',
+        method: 'post',
+        data
+    })
+}
--- src/api/requesturl.js
+++ src/api/requesturl.js
@@ -1,8 +1,23 @@
 const requesturl = {
-    'departmentList':'/department/list',
-    'departmentListDelete':'/department/delete/',
-    'departmentAdd':'/department/add',
-    'departmentEdit':'/department/edit',
+    // department
+    'departmentList':'/department/list',                 // 列表
+    'getDepartmentList':'/department/departmentList',    // 列表 不带分页
+    'departmentListDelete':'/department/delete/',        // 删除
+    'departmentAdd':'/department/add',                   // 添加
+    'departmentEdit':'/department/edit',                 // 编辑
+    // job
+    'jobList':'/job/list',                 // 列表
+    'jobListDelete':'/job/delete/',        // 删除
+    'jobAdd':'/job/add',                   // 添加
+    'jobEdit':'/job/edit',                 // 编辑
 }
 
 export default requesturl
+
+/**
+ * 添加职位
+ * jobName: ""
+ * status: ""
+ * content: ""
+ * parentId: ""
+ */
