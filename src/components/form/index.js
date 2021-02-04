757615bfd7a944fcf98e6face426a25419a05194
xunian <xngs1507179@163.com>
2021-02-04 22:49:08 +0800

    下拉选择框

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
-import { Form, Input, Select, Button, InputNumber, Radio, message } from 'antd'
+import { Form, Input, Select, Button, InputNumber, Radio, message, DatePicker } from 'antd'
+import 'moment/locale/zh-cn'
+import locale from 'antd/es/date-picker/locale/zh_CN'
 import { formSubmit } from '@api/common'
 import requestUrl from "@api/requesturl"
 import PropTypes from 'prop-types'
 import FormSearch from '../formSearch'
+import SelectComponent from '../select/index'
 
 const { Option } = Select
 
@@ -25,6 +28,10 @@ class FormComponent extends Component{
 
     }
 
+    componentWillReceiveProps({ formConfig }){
+        this.refs.form.setFieldsValue(formConfig.setFieldValue)
+    }
+
     rules = (item) => {
         const { messagePrefix } = this.state
         let rules = []
@@ -37,6 +44,14 @@ class FormComponent extends Component{
         }
     }
 
+    validatorSelect = (rule, value) => {
+        console.log(value)
+        if (!value || !value[rule.field]) {
+            return Promise.reject("选项不能为空")
+        }
+        return Promise.resolve()
+    }
+    
     inputElement = (item) => {
         const rules = this.rules(item)
         return (
@@ -57,6 +72,15 @@ class FormComponent extends Component{
                         })
                     }
                 </Select>
+            </Form.Item>
+        )
+    }
+
+    selectComponentElement = (item) => {
+        // const rules = this.rules(item)
+        return (
+            <Form.Item label={item.label} name={item.name} key={item.name} rules={item.rules || []}>
+                <SelectComponent url={item.url} props={item.param} />
             </Form.Item>
         )
     }
@@ -83,18 +107,53 @@ class FormComponent extends Component{
         )
     }
 
+    // 插槽
+    slotElement = (item) => {
+        const rules = this.rules(item)
+        return (
+            <Form.Item label={item.label} name={item.name} key={item.name} rules={ rules }>
+                { this.props.children ? this.props.children.filter(elem => console.log(elem.ref)) : "" }
+                {/* { this.props.children ? this.props.children.filter(elem => elem.ref === item.slotName)[0] : "" } */}
+                { console.log(Array.isArray(this.props.children)) }
+                { this.props.children && Array.isArray(this.props.children) ? this.props.children.filter(elem => console.log(elem.ref)) : this.props.children }
+            </Form.Item>
+        )
+    }
+
+    columnElement = (item) => {
+        const rules = this.rules(item)
+        return (
+            <div class="form-column">
+                <h4>{ item.label }</h4>
+            </div>
+        )
+    }
+
+    dateElement = (item) => {
+        const rules = this.rules(item)
+        return (
+            <Form.item label={item.label} name={item.name} key={item.name} rules={rules}>
+                <DatePicker locale={locale} format={item.format} picker={item.mode}/>
+            </Form.item>
+        )
+    }
+
     initFormItem = (item) => {
         const { formItem } = this.props
         if (!formItem || (formItem && formItem.length === 0)) {
             return false
         }
         const formItemList = []
-        formItem.map(item => {
+        // map, filter, reduce 需要返回值 forEach不需要返回值
+        formItem.forEach(item => {
             if (item.type === 'Input') {
                 formItemList.push(this.inputElement(item));
             }
             if (item.type === 'Select') {
                 formItemList.push(this.selectElement(item));
+            }
+            if (item.type === 'SelectComponent') {
+                formItemList.push(this.selecComponenttElement(item));
             }
             if (item.type === 'InputNumber') {
                 formItemList.push(this.inputNumberElement(item));
@@ -102,14 +161,54 @@ class FormComponent extends Component{
             if (item.type === 'Radio') {
                 formItemList.push(this.radioElement(item));
             }
+            if (item.type === 'Slot') {
+                formItemList.push(this.slotElement(item));
+            }
+            if (item.type === 'Column') {
+                formItemList.push(this.columnElement(item));
+            }
+            if (item.type === 'Date') {
+                formItemList.push(this.dateElement(item));
+            }
         })
         return formItemList
     }
 
+    formatData = (value) => {
+        
+        // 请求的原始数据,深度拷贝
+        const requestData = JSON.parse(JSON.stringify(value))
+        console.log(value)
+
+        const { formatFormKey, editKey, setFieldValue } = this.props.formConfig
+
+        // const jsonKey = this.props.formConfig.formatFormKey
+        const keyValue = requestData[formatFormKey]
+        console.log(formatFormKey)
+        console.log(keyValue)
+        // 如果是json对象
+        // == 匹配数据是否相等，=== 匹配数据及数据类型
+        if (Object.prototype.toString.call(keyValue) === "[ object Object]") {
+            console.log(keyValue)
+            requestData[formatFormKey] = keyValue[formatFormKey]
+        }
+
+        // 判断是否存在"编辑"状态指定的key
+        if (editKey) {
+            requestData[editKey] = setFieldValue[editKey]
+        }
+        return requestData
+    }
+
     // add edit
     onSubmit = (value) => {
-        this.props.onSubmit(value)
+        if (this.props.submit) {
+            this.props.submit(value)
+            return false
+        }
 
+        // const requestData = this.formatData(value)
+        // return false
         const data = {
             url: requestUrl[this.props.formConfig.url],
             data: value
@@ -133,6 +232,9 @@ class FormComponent extends Component{
         return(
             <Form ref="form" onFinish={this.onSubmit} initialValues={{ status:true,number:0 }} {...this.props.formLayout}>
                 { this.initFormItem() }
+                {/* 接收插槽内容 */}
+                { this.props.children }
+                console.log({ this.props.children })
                 <Form.Item >
                         <Button loading={this.state.loading} htmlType="submit" type="primary" >确定</Button>
                 </Form.Item>
