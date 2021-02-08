d9c6506dc196a8cbe2b2152419f587c2c0d19a39
xunian <xngs1507179@163.com>
2021-02-09 00:33:20 +0800

    用户编辑

--- src/views/user/components/userModal.js
+++ src/views/user/components/userModal.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-import { UserAdd, UserDetailed } from '@/api/user'
-import { message, Modal } from 'antd'
+import { UserAdd, UserDetailed, UserEdit } from '@/api/user'
+import { Checkbox, message, Modal } from 'antd'
 import FormComponent from '@c/form/index'
 import { validate_phone } from '@/utils/validate'
 import CryptoJs from 'crypto-js'
@@ -11,6 +11,10 @@ class UserModal extends Component {
         this.state = {
             isModalVisible: false,
             user_id: "",
+            role_options: [
+                {label: "aa", value: 1},
+                {label: "aa", value: 1}
+            ],
             password_rules: [
                 () => ({
                     validator(rule, value) {
@@ -73,9 +77,8 @@ class UserModal extends Component {
                     required: false, 
                     style: { width: '200px'},
                     placeholder: '请输入姓名',
-                    rules: [
-                        { min: 5,message: "不能小于5个字符"}
-                    ]
+                    rules: "",
+                    blurEvent: true
                 },
                 { 
                     type: "Input", 
@@ -86,7 +89,8 @@ class UserModal extends Component {
                     required: false, 
                     style: { width: '200px'},
                     placeholder: '请输入姓名',
-                    rules: ""
+                    rules: "",
+                    blurEvent: true
                 },
                 { 
                     type: "Input", 
@@ -148,15 +152,16 @@ class UserModal extends Component {
         // this.updateArrayItem(1,"required",id ? false : true)
         // this.updateArrayItem([1,2],"required",id ? false : true)
         //  this.updateArrayItem([1,2],{required: id ? false : true,rules: "111"})
+        // 编辑的时候不校验？比较好的做法是修改密码单独做一个功能，或者校验成功才修改密码
         this.updateArrayItem(
             [1,2],
             {
                 1 : {
-                    required: id ? false : true,
+                    // required: id ? false : true,
                     rules: id ? "" : this.state.password_rules
                 },
                 2 : {
-                    required: id ? false : true,
+                    // required: id ? false : true,
                     rules: id ? "" : this.state.passwordconfirm_rules
                 }
 
@@ -199,6 +204,19 @@ class UserModal extends Component {
 
     }
 
+    onBlurEvent = (e) => {
+        const value = e.currentTarget.value
+        if (value) {
+            this.updateItem(value ? false: true)
+        }
+        // if (e.currentTarget.id === 'password' && this.state.user_id) {
+        //     this.updateArrayItem([1], { 1: {rules: ""}})
+        // }
+        // if (e.currentTarget.id === 'passwordconfirm' && this.state.user_id) {
+        //     this.updateArrayItem([2], { 2: {rules: ""}})
+        // }
+    }
+
     handleCancel = () => {
         this.child.onReset()
         this.visibleModal(false)
@@ -215,10 +233,76 @@ class UserModal extends Component {
         })
     }
 
+    handlerFormAdd = (value) => {
+        const requestData = value
+        requestData.password = CryptoJs.MD5(value.password).toString()
+        
+        delete requestData.password
+        UserAdd(requestData).then(res => {
+            const resData = res.data
+            message.info(resData.message)
+            this.handleCancel(false)
+        })
+    }
+
+    handlerFormEdit = (value) => {
+        
+        const password = value.password
+        const passwordconfirm = value.passwordconfirm
+
+        if (password) {
+            if (!validate_password(password)) {
+                message.info("请输入6-20位的英文和数字的密码")
+                return false
+            }
+        }
+
+        if (!passwordconfirm) {
+            if (!validate_password(passwordconfirm)) {
+                message.info("请输入6-20位的英文和数字的确认密码密码")
+                return false
+            }
+        }
+
+        if ((password && passwordconfirm) || (password && !passwordconfirm) || (!password && passwordconfirm)) {
+            if (password !== passwordconfirm) {
+                message.info("两次密码不一致")
+                return false
+            }
+        }
+
+        const requestData = value
+        requestData.id = this.state.user_id
+        if (requestData.password) {
+            requestData.password = CryptoJs.MD5(value.password).toString()
+            delete requestData.passwordconfirm
+        }
+
+        // 提交前验证密码，不再是输入的时候验证，交互体验的取舍问题
+        return false
+        delete requestData.password
+        UserEdit(requestData).then(res => {
+            const resData = res.data
+            message.info(resData.message)
+            this.handleCancel(false)
+        })
+    }
+
+    onChangeRole = (value) => {
+        console.log(value)
+    }
+
     render(){
         return (
-            <Modal title="Basic Modal" visible={this.state.isModalVisible} onOk={this.handleOk} onCancel={this.handleCancel}>
-                <FormComponent formItem={this.state.formItem} formLayout={this.state.formLayout} formConfig={this.state.formConfig} submit={this.onHandlerSubmit} submitButton={false}/>
+            <Modal title="新增用户" visible={this.state.isModalVisible} onOk={this.handleOk} onCancel={this.handleCancel}>
+                <FormComponent onRef={this.onFormRef} onBlur={this.onBlurEvent} formItem={this.state.formItem} formLayout={this.state.formLayout} formConfig={this.state.formConfig} submit={this.onHandlerSubmit} submitButton={false}/>
+                <div ref="role">
+                    <Checkbox.Group 
+                        options={this.state.role_options}
+                        defaultValue={['Apple']}
+                        onChange={this.onChangeRole}
+                    />
+                </div>
             </Modal>
         )
     }
